4ebaa881724321f22e3b134cb76609b6bb3ff570
sssbug <59488967+sssbug@users.noreply.github.com>
2021-06-21 14:59:43 +0300

    Start

---
+++ WebSiteProject/js/Page.js
@@ -0,0 +1,12 @@
+const allText = document.querySelector(".h1");
+
+function reText() {
+   for (let i = 0; i < allText.length; i++) {
+       if (allText.textContent == 10){
+           console.log(dolu);
+       }
+        
+   }
+}
+
+reTex
